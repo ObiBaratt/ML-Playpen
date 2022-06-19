4fd1de8bb0a9ab07d5b41e654b91a7eb20aa6326
Obi Baratt <obibaratt@gmail.com>
2022-06-19 15:21:41 -0700

    Add sklearn sentiment analysis model with route.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, render_template, request
-
 from textblob import TextBlob
+from joblib import load
 
 
 def basic_text_sentiment(text):
@@ -10,10 +10,13 @@ def basic_text_sentiment(text):
 
 app = Flask(__name__)
 
+sk_model = load('sk_sentiment.joblib')
+
 
 @app.route('/')
 def index():
-    return f"<a href='{url_for('text_sentiment')}'>Text Sentiment Analysis</a>"
+    return f"<a href='{url_for('text_sentiment')}'>Text Sentiment Analysis using TextBlob</a>" \
+           f"<a href='{url_for('sk_sentiment')}'>Text Sentiment Analysis with sklearn</a>"
 
 
 @app.route('/text', methods=['GET', 'POST'])
@@ -22,6 +25,20 @@ def text_sentiment():
         input_text = request.form['text_input']
         analyzed = basic_text_sentiment(input_text)
         return render_template('text.html', analyzed=analyzed, display=input_text)
+    return render_template('text.html')
+
+
+@app.route('/sk_sentiment', methods=['GET', 'POST'])
+def sk_sentiment():
+    if request.method == 'POST':
+        input_text = request.form['text_input']
+        iterable_text = [input_text]
+        prediction_val = sk_model.predict(iterable_text)
+        if prediction_val == 1:
+            prediction = 'Positive'
+        else:
+            prediction = 'Negative'
+        return render_template('text.html', analyzed=prediction, display=input_text)
     return render_template('text.html')
 
 
