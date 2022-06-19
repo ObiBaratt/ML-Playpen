9dc3df9d7694c04a9af3e0e4106e08d1ff308366
Obi Baatt <obibaratt@gmail.com>
2022-06-18 22:35:16 -0700

    Add files via upload initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, url_for, render_template, request
+
+from textblob import TextBlob
+
+
+def basic_text_sentiment(text):
+    blob = TextBlob(text)
+    return blob.sentiment
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return f"<a href='{url_for('text_sentiment')}'>Text Sentiment Analysis</a>"
+
+
+@app.route('/text', methods=['GET', 'POST'])
+def text_sentiment():
+    if request.method == 'POST':
+        input_text = request.form['text_input']
+        analyzed = basic_text_sentiment(input_text)
+        return render_template('text.html', analyzed=analyzed)
+    return render_template('text.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
